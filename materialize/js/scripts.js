524f704f1414af7868a96d21ce89de289fa0b331
Dattatreya Nayak <dattu4621@gmail.com>
2017-02-19 00:10:41 +0530

    Update scripts.js

--- materialize/js/scripts.js
+++ materialize/js/scripts.js
@@ -1,20 +1,18 @@
 
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('#LoginModal').modal();
-    $('#SignUpModal').modal();
+    //$('#LoginModal').modal();
+    //$('#VideoModal').modal();
+    $('.modal').modal();    
+    //$('.carousel').carousel();
     $('.carousel-slider').carousel({fullWidth: true});        
     $('.carousel-slider').slider({full_width: true});//slider init
     autoplay();
-
+    //$('.modal-trigger').leanModal();
     function autoplay() {
       $('.carousel').carousel('next');
       setTimeout(autoplay, 5000);
     };
-    $('.parallax').parallax();
     
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-      });
+
   });
