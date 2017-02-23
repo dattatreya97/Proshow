852806f351ee03cb42742bb9d4722538dc8fd4ad
Dattatreya Nayak <dattu4621@gmail.com>
2017-02-23 10:17:43 +0530

    Update scripts.js

--- materialize/js/scripts.js
+++ materialize/js/scripts.js
@@ -1,18 +1,19 @@
 
+
 $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    //$('#LoginModal').modal();
-    //$('#VideoModal').modal();
+    $('#ticket').hide();
     $('.modal').modal();    
     //$('.carousel').carousel();
     $('.carousel-slider').carousel({fullWidth: true});        
     $('.carousel-slider').slider({full_width: true});//slider init
-    autoplay();
-    //$('.modal-trigger').leanModal();
+    autoplay();    
     function autoplay() {
       $('.carousel').carousel('next');
       setTimeout(autoplay, 5000);
     };
-    
-
+    $('#IDModal').modal('open');    
+     $('#admit').click(function(){
+        $('#ticket').toggle("slow");
+     });
+     
   });
